efe4f2496628981ecdc31c54a75572a9e50c0c1b
Kyle Patrick Bader <kbader91@gmail.com>
2022-05-24 19:07:26 -0700

    initial components commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
+// importing components from react-router-dom package
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+// components 
+import Home from "./components/Home/home";
+import Nav from "./components/Nav/nav";
+import Signup from "./components/Signup/signup";
+import Profile from "./components/Profile/profile";
+import Contact from "./components/Contact/contact";
+
 function App() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <>
+    `<Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+      </Router>`
+    </>
   );
 }
 
