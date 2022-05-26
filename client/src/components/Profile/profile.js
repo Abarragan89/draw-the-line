005b84e9f096461824c95925d330ed2c6ed82988
Anthony Barragan <anthony.bar.89@gmail.com>
2022-05-25 20:12:40 -0700

    test: rebase

--- client/src/components/Profile/profile.js
+++ client/src/components/Profile/profile.js
@@ -5,7 +5,6 @@ function Profile() {
         <>
             <p>This is my profile page with all my posts, friends, ability to make a post, and links to my friends profiles pages</p>
             <Link to="/Create-post">Make a new post!</Link>
-
         </>
     )
 }
