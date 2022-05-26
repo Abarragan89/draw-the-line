2db47ace3882e10338d0e949e83345966e16710c
Anthony Barragan <anthony.bar.89@gmail.com>
2022-05-25 20:58:24 -0700

    test/added seeds

---
+++ server/seeders/seeds.js
@@ -0,0 +1,28 @@
+// const faker = require('faker');
+const userSeeds = require('./userSeeds.json');
+const postSeeds = require('./postSeeds.json');
+const db = require('../config/connection');
+const { Post , User, Comment } = require('../models');
+db.once('open', async () => {
+  try {
+    await Post.deleteMany({});
+    await User.deleteMany({});
+    await User.create(userSeeds);
+    for (let i = 0; i < postSeeds.length; i++) {
+      const { _id, postAuthor } = await Post.create(postSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: postAuthor },
+        {
+          $addToSet: {
+            posts: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('all done!');
+  process.exit(0);
+});
