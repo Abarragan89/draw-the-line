1b6e6052893ba50e7e605f399d0e66e7d5c6b287
Kyle Patrick Bader <kbader91@gmail.com>
2022-05-28 15:54:26 -0700

    updated nav tabs, css and search friends css

--- client/src/components/Profile/profile.js
+++ client/src/components/Profile/profile.js
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom'
+import Nav from '../Nav/nav';
 
 function Profile() {
     return (
         <>
+            <div id="hamburger">
+                <Nav />
+            </div>
+
             <p>This is my profile page with all my posts, friends, ability to make a post, and links to my friends profiles pages</p>
             <Link to="/Create-post">Make a new post!</Link>
         </>
--- client/src/components/SearchPeople/searchPeople.js
+++ client/src/components/SearchPeople/searchPeople.js
@@ -1,3 +1,6 @@
+import './searchPeople.css';
+import Nav from '../Nav/nav';
+
 import { QUERY_USER_BY_NAME } from '../../utils/queries';
 import { useLazyQuery } from '@apollo/client';
 import { ADD_FRIEND } from '../../utils/mutations';
@@ -11,21 +14,24 @@ function SearchPeople() {
 
     return (
         <>
-            <form onSubmit={ async (e)=> {
+            <div id="hamburger">
+                <Nav />
+            </div>
+            <form id="search-friend-form" onSubmit={async (e) => {
                 e.preventDefault();
                 const inquiryUser = await document.getElementById('search-bar').value
-                await findUser({variables: {username: inquiryUser}})
-                }   
+                await findUser({ variables: { username: inquiryUser } })
+            }
             }>
-                <label>Search for People</label>
-                <input type='text' name='search-bar' id='search-bar'/>
-                <button type='submit'>Search</button>
-                { data && data.userByName === null && <p>User does not exist</p>}
-                { data && data.userByName !== null && 
-                <>
-                    <p>{data.userByName.username}</p>
-                    <button>Add Friend</button>
-                </>
+                <label id="sfp-label">Search for People</label>
+                <input id='search-bar' type='text' name='search-bar' />
+                <button id="search-friend-btn" type='submit'>Search</button>
+                {data && data.userByName === null && <p>User does not exist</p>}
+                {data && data.userByName !== null &&
+                    <>
+                        <p>{data.userByName.username}</p>
+                        <button id="add-friend-btn" type='submit'>Add Friend</button>
+                    </>
                 }
             </form>
             <div>
