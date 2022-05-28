b5f412a35b26fc800b29bf624a486994e50636be
unknown <christiandc19@hotmail.com>
2022-05-28 16:19:57 -0700

    create a new route for single post page

--- client/src/components/SinglePost/SinglePost.js
+++ client/src/components/SinglePost/SinglePost.js
@@ -1,35 +1,10 @@
-import { useParams, Link } from 'react-router-dom';
-import { QUERY_FRIEND } from '../../utils/queries';
-import { useQuery } from '@apollo/client';
 
 function SinglePost () {
-
-    const { id: userId } = useParams()
-    const { loading, data } = useQuery(QUERY_FRIEND, {
-        variables: { id: userId },
-      });
-    
-    const userPosts = data?.user.posts || [];
-    const userFriends = data?.user.friends || [];
 
     return (
         <>  
             <section>
-                <h1>Posts</h1>
-                {userPosts.map((post, index) => (
-                    <section key={index}>
-                        <h3>{post.postTitle}</h3>
-                        <p>{post.postText}</p>
-                    </section>
-                ))}
-            </section>
-            <section>
-                <h1>Friends</h1>
-                {userFriends.map((friend, index) => (
-                    <div key={index}>
-                       <Link to={`/friendprofile/${friend._id}`}>{friend.username}</Link>
-                    </div>
-                ))} 
+                <h1>Single Post Section</h1>
             </section>
         </>
     )
