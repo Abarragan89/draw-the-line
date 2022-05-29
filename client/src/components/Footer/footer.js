55f12c4d909cb12e10913d9d46f8c8a8967c0f3c
sophie homer <sophiehomer94@gmail.com>
2022-05-28 17:04:22 -0700

    rebasing

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -11,7 +11,6 @@ const Footer = () => {
       <footer>
           <a href='https://www.instagram.com/draw_the_line_co/' className='instagramIcon'><AiOutlineInstagram size={25}/></a>
           <a href='https://twitter.com/DrawTheLineCo' className='twitterIcon'><AiOutlineTwitter size={25}/></a>
-          <a href='https://stackoverflow.com/users/17585483/sophie-homer' className='stackOverflowIcon'><FaStackOverflow size={25}/></a>
           <a href='mailto:drawthelinecompany@gmail.com' className='emailIcon'><AiFillMail size={25}/></a>
       </footer>
   )
