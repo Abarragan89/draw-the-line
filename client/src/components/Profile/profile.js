0708fdb6c1c0a79c281354407244d31dc49546fa
Anthony Barragan <anthony.bar.89@gmail.com>
2022-05-29 10:39:08 -0700

    test: working on censoring language

--- client/src/components/Profile/profile.js
+++ client/src/components/Profile/profile.js
@@ -6,6 +6,11 @@ import { useQuery, useMutation } from '@apollo/client';
 import './postCard.css'
 import { ADD_POST } from '../../utils/mutations';
 
+// Bad word Filter
+var Filter = require('bad-words'),
+    filter = new Filter();
+
+
 function Profile () {
 
     const { id: userId } = useParams()
@@ -30,7 +35,17 @@ function Profile () {
 
     // update state based on form input changes
     const handleChange = (event) => {
-        const { name, value } = event.target;
+        let { name, value } = event.target;
+        
+        name = filter.clean(name)
+        value = filter.clean(value)
+
+        if (value.match(/([*]{3,})/g)) {
+            const warningDiv = document.getElementById('bad-words-warning');
+            warningDiv.innerHTML = 'Let\'s be nice';
+            // const postBtn = document.getElementsByClassName('post-btn');
+            // postBtn.setAttribute('disabled', '');
+        }
         setFormState({
         ...formState,
         [name]: value,
@@ -38,10 +53,23 @@ function Profile () {
     };
 
     const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    await addPost({
-        variables: { ...formState },
-    });
+        event.preventDefault();
+        let postText = document.getElementById('postText').value;
+        let postTitle = document.getElementById('postTitle').value;
+
+        console.log(postText)
+        postTitle = filter.clean(postTitle)
+        postText = filter.clean(postText)
+
+        console.log(postText)
+
+        setFormState({
+            [postTitle]: postText,
+            });
+
+        await addPost({
+            variables: { ...formState },
+        });
     };
 
 
@@ -53,7 +81,8 @@ function Profile () {
             <section>
             <input className='post-tile' type="text" id="postTitle" name="postTitle" value={formState.postTitle} onChange={handleChange} placeholder='Write Title Here' />
             <input type="text" id="postText" name="postText" value={formState.postText} onChange={handleChange} placeholder='Write Post Here' />
-            <div btn-container><button className='post-btn'>POST</button></div>
+            <div id="bad-words-warning"></div>
+            <div><button className='post-btn'>POST</button></div>
             </section>
             </form>
 
